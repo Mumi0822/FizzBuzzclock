2876d065c8002045483b74ce13baf567abb27288
apple <apple@applenoMacBook-Air.local>
2021-08-06 17:09:25 +0900

    時計機能は実装できた

--- firstapp/sorce/fizzbuzz.js
+++ firstapp/sorce/fizzbuzz.js
@@ -3,20 +3,33 @@ const fizzbuzz ={
     Check(number){
         this.result="";//初期化
         if(number%3==0||number%5==0){
-            if(number%3==0){
-                this.result = this.result + "fizz";
-                
-            }
-            if(number%5==0){
-                this.result= this.result + "buzz";
-            }
+            this.result =  number%3==0 ? this.result + "fizz":this.result;
+            this.result =  number%5==0 ? this.result + "buzz":this.result;
             return this.result;
         }else{
+            number = number < 10 ? "0" + number : number;
             return number;
         }
     }
 };
+const Clock = function(){
+    const d = new Date();
+    // 時を取得
+    let hour = d.getHours();
+    // 分を取得
+    let min = d.getMinutes();
+    // 秒を取得
+    let sec = d.getSeconds();
+  
+    // 1桁の場合は0を足して2桁に
+    hour = fizzbuzz.Check(hour);
+    min = fizzbuzz.Check(min);
+    sec = fizzbuzz.Check(sec);
+  
+    // 日付・時刻の文字列を作成
+    let time = `${hour}:${min}:${sec}`;
+    document.querySelector(".clock-time").innerText = time;
+};
+setInterval(Clock, 1000);
 
-for(i=1;i<=100;i++){
-    console.log(fizzbuzz.Check(i));
-}
+
